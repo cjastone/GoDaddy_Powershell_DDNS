fe851495118c202f1003fd2a21496fc39bbf6d1e
Chris Stone <cjastone@users.noreply.github.com>
2018-09-25 10:47:12 +1000

    Update godaddy_ddns.ps1

--- godaddy_ddns.ps1
+++ godaddy_ddns.ps1
@@ -1,30 +1,32 @@
-#
-This script is used to check and update your GoDaddy DNS server to the IP address of your current internet connection.
+<#
+godaddy_ddns.ps1
 
-First go to GoDaddy developer site to create a developer account and get your key and secret
+Checks and updates a nominated GoDaddy DNS A record to your current IP address of your current Internet connection.  
 
-https://developer.godaddy.com/getstarted
- 
-Update the first 4 varriables with your information
- 
+A fork of the original project at https://github.com/markafox/GoDaddy_Powershell_DDNS which at the time of forking was non-functional.
+
+Requires GoDaddy developer account to obtain key and secret: https://developer.godaddy.com/getstarted
+
+To use, simply update the first four variables with your domain, A record, API key and API secret.
 #>
-$domain = 'your.domain.to.update'  # your domain
-$name = 'name_of_host' #name of the A record to update
-$key = 'key' #key for godaddy developer API
-$secret = 'Secret' #Secret for godday developer API
 
+$domain = 'yourdomain.com'	 # domain name to update
+$name = 'your.subdomain'  	 # A record to update
+$key = 'yourapikey'		       # key for GoDaddy Developer API
+$secret = 'yourapisecret'	  # secret for GoDaddy Developer API
+
+# build request headers and perform lookup of current address of nominated A record
 $headers = @{}
 $headers["Authorization"] = 'sso-key ' + $key + ':' + $secret
-$result = Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -method get -headers $headers
+$result = Invoke-WebRequest -uri "https://api.godaddy.com/v1/domains/$domain/records/A/$name" -method "GET" -headers $headers
 $content = ConvertFrom-Json $result.content
-$dnsIp = $content.data
+$current_ip = $content.data
 
-# Get public ip address there are several websites that can do this.
-$currentIp = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
+# obtain current external IP address
+$new_ip = Invoke-RestMethod -uri "http://ipinfo.io/json" | Select -exp ip
 
-if ( $currentIp -ne $dnsIp) {
-    $Request = @{ttl=3600;data=$currentIp }
-    $JSON = Convertto-Json $request
-    Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -method put -headers $headers -Body $json -ContentType "application/json"
+# check exernal IP against DNS record and update if necessary
+if ( $new_ip -ne $current_ip) {
+    $body = ConvertTo-Json @(@{data=$new_ip;ttl=3600})
+    Invoke-WebRequest -uri "https://api.godaddy.com/v1/domains/$domain/records/A/$name" -method "PUT" -headers $headers -Body $body -ContentType "application/json"
 }
-
